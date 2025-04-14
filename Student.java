9fd497977638d9bcb1177f3f53c749a44da8afbb
PRITHVIJIT BOSE <boseprithvijit@gmail.com>
2025-04-15 01:54:09 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    private int id;
+    private String name;
+    private double grade;
+
+    public Student(int id, String name, double grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getGrade() { return grade; }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Name: " + name + " | Grade: " + grade;
+    }
+}
